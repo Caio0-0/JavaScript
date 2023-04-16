40132307e22c5c83142396bad116f2b5f32e1cb0
Caio0-0 <126476942+Caio0-0@users.noreply.github.com>
2023-04-16 15:44:08 -0300

    novos ex

---
+++ Aulas/A-008/A-008.js
@@ -0,0 +1,9 @@
+var vel = 64
+
+console.log(`A velocidade do seu caro é ${vel}Km/h`)
+if (vel > 60){
+    console.log(`Você utrapassou o limite de velocidade.MULTADO!`)
+}else{
+    console.log(`Voce esta na velocidade adequada`)
+}
+console.log(`Dirija sempre com cinto de aegurança `)
