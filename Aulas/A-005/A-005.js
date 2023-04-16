171447436edeccabda2851261f7d5cd979b8360b
Caio0-0 <126476942+Caio0-0@users.noreply.github.com>
2023-04-16 10:19:00 -0300

    novo

---
+++ Aulas/A-005/A-005.js
@@ -0,0 +1,5 @@
+var corpo = window.document.body
+var msm = window.document.getElementById("Mensagem")
+var p1 = window.document.getElementsByTagName('p')[0]
+msm.style.background = 'green'
+p1.style.color = 'red'
